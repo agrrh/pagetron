9dbc3b3322013be5f7b2e78b258442d40be2e3ea
Kirill K <kovalev.kirill.a@gmail.com>
2023-12-03 03:28:25 +0300

    feat(front): integrate overview

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,11 +3,28 @@
 	import Overview from '$lib/Overview.svelte';
 	import ComponentsList from '$lib/ComponentsList.svelte';
 	import Footer from '$lib/Footer.svelte';
+	import Dummy from '$lib/Dummy.svelte';
+
+	async function overview() {
+		const response = await fetch(`http://127.0.0.1:3000/overview/`);
+		const overview = await response.json();
+		return overview;
+	}
 </script>
 
 <Header />
 
-<Overview />
-<ComponentsList />
+{#await overview()}
+	<Dummy />
+{:then overview}
+	<Overview
+		status={overview.status}
+		componentsCount={overview.components_count}
+		issuesCount={overview.issues_count}
+		componentsIssues={overview.components_issues}
+		datetimeHuman={overview.datetime_human}
+	/>
+	<ComponentsList />
+{/await}
 
 <Footer />
