a58a01243d744bcd3cb0b29537f78b3e7b1a5b2f
Kirill K <kovalev.kirill.a@gmail.com>
2023-12-09 20:10:00 +0300

    fix: api prefix trailing slash

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,1 +1,1 @@
-export const apiUrl = import.meta.env.DEV ? "http://localhost:3000" : (import.meta.env.VITE_API_URL || "/api/");
+export const apiUrl = import.meta.env.DEV ? "http://localhost:3000" : (import.meta.env.VITE_API_URL || "/api");
