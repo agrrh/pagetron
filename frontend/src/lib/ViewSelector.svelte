568be63cac373310c2356731c565beb1977f50d8
Kirill K <kovalev.kirill.a@gmail.com>
2023-12-09 21:07:50 +0300

    refactor: improve viewSelector

--- frontend/src/lib/ViewSelector.svelte
+++ frontend/src/lib/ViewSelector.svelte
@@ -8,41 +8,34 @@
 	let isActiveQuarter = "";
 	let isActiveYear = "";
 
-	const views = [
-		{ name: 'hours', icon: 'fa fa-clock' },
+	let view = '';
+	let views = [
+		{ name: 'year', icon: 'fa fa-earth-asia' },
 		{ name: 'quarter', icon: 'fa fa-calendar-days' },
-		{ name: 'year', icon: 'fa fa-earth-asia' }
+		{ name: 'hours', icon: 'fa fa-clock' },
 	];
 
-	function nextView(e) {
-		let viewsList = views.map((x) => x.name);
-		let i = viewsList.indexOf(view) + 1;
-
-		if (i >= views.length) {
-			i = 0;
-		}
-
+	function cycleViews(e) {
 		if (view != null) {
-			viewStore.set(viewsList[i]);
+			viewStore.set(views[2]['name']);
 		}
-	}
 
-	let view = '';
+		views = [
+			views[1],
+			views[2],
+			views[0],
+		]
+	}
 
 	viewStore.subscribe((value) => (view = value));
 
-	$: curViewIconClass = views.filter((x) => x.name == view)[0]['icon'];
-	$: viewText = views.filter((x) => x.name == view)[0]['text'];
-
-	$: isActiveHours = view == 'hours' ? "" : "has-text-grey";
-	$: isActiveQuarter = view == 'quarter' ? "" : "has-text-grey";
-	$: isActiveYear = view == 'year' ? "" : "has-text-grey";
+	$: views;
 </script>
 
 <div class="navbar-item">
-	<a id="viewSelector" class="button is-dark" on:click={nextView}>
-		<span class="fa fa-clock {isActiveHours}"></span>
-		<span class="fa fa-calendar-days mr-3 ml-3 {isActiveQuarter}"></span>
-		<span class="fa fa-earth-asia {isActiveYear}"></span>
+	<a id="viewSelector" class="button is-dark" on:click={cycleViews} transition:fade={options}>
+		<span class="{views[0]['icon']} has-text-grey"></span>
+		<span class="{views[1]['icon']} mr-3 ml-3 is-size-5"></span>
+		<span class="{views[2]['icon']} has-text-grey"></span>
 	</a>
 </div>
