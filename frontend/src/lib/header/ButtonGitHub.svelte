eb13fc3d157ff08dbba83d4149a1995ceac8f005
Kirill K <agrrh@users.noreply.github.com>
2024-12-28 16:24:09 +0300

    feat(ui): improve view (#68)

---
+++ frontend/src/lib/header/ButtonGitHub.svelte
@@ -0,0 +1,10 @@
+<div class="navbar-item">
+	<a
+		class="button is-white is-outlined"
+		href="https://github.com/agrrh/pagetron"
+		target="_blank"
+	>
+		<span class="fab fa-github"></span>
+		<span class=""> &nbsp; GitHub </span>
+	</a>
+</div>
